bf81c82144eed8beecbce869544a0ffbd60923d5
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2020-11-28 19:52:11 +0200

    Implement Sell & amended Quote
    - edited Quote to adapt with Sell
    - db table "stock" >> "transactions"
    - added a lot of comments

--- app.py
+++ app.py
@@ -53,23 +53,29 @@ Show portfolio of stocks
 @app.route("/")
 @login_required
 def index():
+    # Get user ID  and username
     user_id = session.get("user_id")
+    username = db.execute(
+        "SELECT username FROM users WHERE id = :uid", uid=user_id)[0]["username"]
+    # Get current user cash
     user_cash = db.execute(
         "SELECT cash FROM users WHERE id = :uid", uid=user_id)[0]["cash"]
+    # Get current owned shares
     user_assets = db.execute(
-        "SELECT symbol, name, SUM(shares) FROM stock WHERE userID = :uid GROUP BY symbol", uid=user_id)
+        "SELECT symbol, name, SUM(shares) FROM transactions WHERE userID = :uid GROUP BY symbol ORDER BY symbol", uid=user_id)
+    # Initiate the total sum of user's cash and the value of all the assets
     total_assets = user_cash
+
+    # If shares are more than 0, display their stock and add their value to the total cash
     for asset in user_assets:
         if asset["SUM(shares)"] > 0:
             stock_info = lookup(asset["symbol"])
-            # return apology(str(stock_info), str(type(user_cash)))
             asset["price"] = usd(stock_info["price"])
             asset["total_cost"] = usd(
                 asset["SUM(shares)"] * stock_info["price"])
             total_assets = total_assets + \
                 (asset["SUM(shares)"] * stock_info["price"])
-    username = db.execute(
-        "SELECT username FROM users WHERE id = :uid", uid=user_id)[0]["username"]
+
     return render_template("index.html", user=username, assets=user_assets, cash=usd(user_cash), total_assets=usd(total_assets))
 
 
@@ -99,13 +105,14 @@ def buy():
             total_cost = shares * stock_info["price"]
             user_cash = db.execute(
                 "SELECT cash FROM users WHERE id = :uid", uid=user_id)[0]["cash"]
-
+            # Ensure user has enough cash
             if user_cash < total_cost:
-                return apology("pooooor!", "not enough money")
+                return apology("not enough money", "pooooor!")
             else:
+                # save transaction in db with its datetime
                 now = datetime.now()
                 db.execute(
-                    "INSERT INTO stock (userID, symbol, name, shares, unit_price, total_cost, dateID) VALUES (:userID, :symbol, :name, :shares, :unit_price, :total_cost, :dateID)",
+                    "INSERT INTO transactions (userID, symbol, name, shares, unit_price, total_cost, dateID) VALUES (:userID, :symbol, :name, :shares, :unit_price, :total_cost, :dateID)",
                     userID=user_id,
                     symbol=stock_info["symbol"],
                     name=stock_info["name"],
@@ -114,9 +121,11 @@ def buy():
                     total_cost=total_cost,
                     dateID=now
                 )
+                # calculate the remainder of the user's cash
                 rest = user_cash - total_cost
                 db.execute("UPDATE users SET cash = :rest WHERE id = :uid",
                            rest=rest, uid=user_id)
+                # Flash a msg when the transaction succeeds, depending on the amount of money left
                 if rest == 0:
                     flash("No more cash!")
                 elif rest < 5:
@@ -209,15 +218,35 @@ Get stock quote.
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
+    user_id = session.get("user_id")
+    username = db.execute(
+        "SELECT username FROM users WHERE id = :uid", uid=user_id)[0]["username"]
     if request.method == "POST":
-        resp = lookup(request.form.get("symbol"))
-        if not resp:
+        # Ensure symbol is valid
+        stock_info = lookup(request.form.get("symbol"))
+        if not stock_info:
             return apology("", "No such stock")
         else:
+            # If user own shares of this stock, show sell button on quote.html
+            owned_shares = db.execute(
+                "SELECT SUM(shares) FROM transactions WHERE userID = :uid AND symbol = :symbol GROUP BY symbol",
+                uid=user_id, symbol=stock_info['symbol']
+            )
             ownShares = False
-            return render_template("quote.html", showResult=True, name=resp['name'], symbol=resp['symbol'], price=resp['price'], ownShares=ownShares)
+            if len(owned_shares) == 1 and owned_shares[0]["SUM(shares)"] > 0:
+                ownShares = True
+
+            return render_template(
+                "quote.html",
+                showResult=True,
+                name=stock_info['name'],
+                symbol=stock_info['symbol'],
+                price=stock_info['price'],
+                ownShares=ownShares,
+                user=username
+            )
     else:
-        return render_template("quote.html")
+        return render_template("quote.html", user=username)
 
 
 """
@@ -267,7 +296,65 @@ Sell shares of stock
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
-    return apology("TODO")
+    user_id = session.get("user_id")
+
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        # If no value provided
+        if not symbol:
+            return apology('', 'please enter a valid symbol')
+
+        stock_info = lookup(symbol)
+        # If symbol is unavailable or invalid
+        if stock_info is None:
+            return apology('', 'No such symbol')
+        else:
+            shares_to_sell = int(request.form.get("shares"))
+            owned_shares = db.execute(
+                "SELECT SUM(shares) FROM transactions WHERE userID = :uid AND symbol = :symbol", uid=user_id, symbol=symbol)[0]["SUM(shares)"]
+            # Ensure number of shares to sell is enough and valid
+            if shares_to_sell > owned_shares:
+                return apology("You don't own that much", "Scammer")
+            elif shares_to_sell < 0:
+                return apology("Invalid number of shares", "try again")
+
+            total_cost = shares_to_sell * stock_info["price"]
+            user_cash = db.execute(
+                "SELECT cash FROM users WHERE id = :uid", uid=user_id)[0]["cash"]
+            # Save transaction in db, with a negative number of shares indicating selling
+            now = datetime.now()
+            db.execute(
+                "INSERT INTO transactions (userID, symbol, name, shares, unit_price, total_cost, dateID) VALUES (:userID, :symbol, :name, :shares, :unit_price, :total_cost, :dateID)",
+                userID=user_id,
+                symbol=stock_info["symbol"],
+                name=stock_info["name"],
+                shares=shares_to_sell-(shares_to_sell*2),
+                unit_price=stock_info["price"],
+                total_cost=total_cost,
+                dateID=now
+            )
+
+            # Adding money for sold shares to user cash balance
+            new_cash = user_cash + total_cost
+            db.execute(
+                "UPDATE users SET cash = :new_cash WHERE id = :uid",
+                new_cash=new_cash, uid=user_id
+            )
+            flash("Successful Process!")
+            return redirect("/")
+
+    # If method = GET
+    else:
+        # Get symbols whose sum of shares more than zero to be displayed in select menu
+        db_symbols = db.execute(
+            "SELECT symbol, SUM(shares) FROM transactions WHERE userID = :uid GROUP BY symbol", uid=user_id)
+        symbols = []
+        for symbol in db_symbols:
+            if symbol["SUM(shares)"] > 0:
+                symbols.append(symbol["symbol"])
+        username = db.execute(
+            "SELECT username FROM users WHERE id = :uid", uid=user_id)[0]["username"]
+        return render_template("sell.html", user=username, symbols=symbols)
 
 
 """
